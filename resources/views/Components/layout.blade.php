c4cb7ff129c00a9cc0de853af066df12fb6bf14d
lucas-dw16 <341524@student.mboutrecht.nl>
2025-09-15 10:20:09 +0200

    3de video gekeken

---
+++ resources/views/Components/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+</head>
+<body>
+    <nav>
+        <x-nav-link></x-nav-link>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+
+   {{ $slot }}
+   
+</body>
+</html>
