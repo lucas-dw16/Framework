3ed5f9efe55b61aaab9a32c4e356b2c7283e3513
lucas-dw16 <341524@student.mboutrecht.nl>
2025-11-06 17:42:20 +0100

    video 17 gekeken

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -17,8 +17,13 @@
           <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input id="title" type="text" name="title" placeholder="Shift Leader" class="block min-w-0 grow bg-white py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+              <input id="title" type="text" name="title" placeholder="Shift Leader" class="block min-w-0 grow bg-white py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required>
             </div>
+
+            @error('title')
+            <p class="text-red-500 text-sm mt-1">{{ $message }} </p>
+            @enderror
+
           </div>
         </div>
 
@@ -26,12 +31,26 @@
           <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
           <div class="mt-2">
             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-              <input id="salary" type="text" name="salary" placeholder="$50,000 Per Year" class="block min-w-0 grow bg-white py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+              <input id="salary" type="text" name="salary" placeholder="$50,000 Per Year" class="block min-w-0 grow bg-white py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required>
             </div>
+             @error('salary')
+            <p class="text-red-500 text-sm mt-1">{{ $message }} </p>
+            @enderror
           </div>
         </div>
       </div>
     </div>
+
+    <div class="mt-10">
+    @if ($errors->any())
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li class="text-red-500 text-sm">{{ $error }}</li>
+        @endforeach
+      </ul>
+    @endif
+    </div>
+
   </div>
 </div>
 
